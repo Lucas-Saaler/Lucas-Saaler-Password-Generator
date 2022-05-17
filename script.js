aef54ac481ecf5991b920a8f7a4bca335f11092c
Lucas Saaler <lucassaaler00@gmail.com>
2022-05-17 19:53:47 -0400

    Added prompts to confirm password length and characters

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = lowerCase.toUpperCase()
+var numbers = "0123456789"
+var special = "!@#$%^&*()-_=+[{]};:,<.>/?"
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +14,17 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var finalPwd = ""
+  var charSelection = prompt("How many characters do you wish your password to contain? (Between 8 and 128)")
+    if (charSelection >= 8 && charSelection <= 128) {
+      var isUpperCase = confirm("Do you want your password to contain capital letters?")
+      var isLowerCase = confirm("Do you want your password to contain lowercase letters?")
+      var isNumber = confirm("Do you want your password to contain numbers?")
+      var isSpecial = confirm("Do you want your password to contain special characters?")
+    }else{
+      alert("Please input a number between 8 and 128.")
+    }
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
