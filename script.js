d02b338712e0c80ca6c1713ee1a73379ce7cd712
Lucas Saaler <lucassaaler00@gmail.com>
2022-05-17 20:31:17 -0400

    Finished adding the ability to generate a new password

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function writePassword() {
 }
 
 function generatePassword() {
+  var protoPassword = ""
   var finalPwd = ""
   var charSelection = prompt("How many characters do you wish your password to contain? (Between 8 and 128)")
     if (charSelection >= 8 && charSelection <= 128) {
@@ -22,9 +23,35 @@ function generatePassword() {
       var isLowerCase = confirm("Do you want your password to contain lowercase letters?")
       var isNumber = confirm("Do you want your password to contain numbers?")
       var isSpecial = confirm("Do you want your password to contain special characters?")
+      for(var i=0; i < charSelection; i++) {
+          if (isUpperCase){
+            var positionStringUpperCase = Math.floor(Math.random() * 26)
+            protoPassword = protoPassword + upperCase[positionStringUpperCase]
+          }
+          if (isLowerCase){
+            var positionStringLowerCase = Math.floor(Math.random() * 26)
+            protoPassword = protoPassword + lowerCase[positionStringLowerCase]
+          }
+          if (isNumber){
+            var positionStringNumber = Math.floor(Math.random() * 9)
+            protoPassword = protoPassword + numbers[positionStringNumber]
+          }
+          if (isSpecial){
+            var positionStringSpecial = Math.floor(Math.random() * 26)
+            protoPassword = protoPassword + special[positionStringSpecial]
+          }
+        }
+      if (protoPassword.length > charSelection){
+        for (var i=0; i < charSelection; i++) {
+          finalPwd = finalPwd + protoPassword[i]
+      }
+    }else{
+      finalPwd = protoPassword
+    }
     }else{
       alert("Please input a number between 8 and 128.")
     }
+    return finalPwd
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
